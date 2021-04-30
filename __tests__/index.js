fea87aa6be23b5589a850bc3903a2469af2ef5f7
twinh <twinhuang@qq.com>
2021-04-30 16:22:06 +0800

    test(api): 初始化测试

---
+++ __tests__/index.js
@@ -0,0 +1,24 @@
+import api from '..';
+import {createPromise, bootstrap} from '@mxjs/test';
+import $ from 'miaoxing';
+
+bootstrap();
+
+describe('test', () => {
+  test('get', async () => {
+    const promise = createPromise();
+    $.http = jest.fn().mockImplementationOnce(() => promise.resolve({
+      data: {
+        foo: 3,
+        bar: 4,
+      },
+    }));
+
+    const ret = await api.get('abc', {loading: true});
+    expect(ret).toMatchSnapshot();
+
+    expect($.http).toHaveBeenCalledTimes(1);
+    expect($.http).toMatchSnapshot();
+  });
+});
+
